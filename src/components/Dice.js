c7694dceca1a705952bafab0a96d9ff39c553eb3
divya569 <85498653+divya569@users.noreply.github.com>
2021-07-02 15:28:46 +0530

    Update Dice.js

--- src/components/Dice.js
+++ src/components/Dice.js
@@ -18,14 +18,15 @@ class Dice extends React.Component{
             start:<h1>Start Game!</h1>
         }
     }
-    clickHandler=()=>{
+    clickHandler=()=>{ 
             var img=[]
-            img[1]=<img style={{height:"100px"}} src={img1} alt="img1"/>
-            img[2]=<img style={{height:"100px"}} src={img2} alt="img2"/>
-            img[3]=<img style={{height:"100px"}} src={img3} alt="img3"/>
-            img[4]=<img style={{height:"100px"}} src={img4} alt="img4"/>
-            img[5]=<img style={{height:"100px"}} src={img5} alt="img5"/>
-            img[6]=<img style={{height:"100px"}} src={img6} alt="img6"/>
+            img[1]=<img style={{height:"100px"}} src={img1} alt="img1" className="img"/>
+            img[2]=<img style={{height:"100px"}} src={img2} alt="img2"className="img"/>
+            img[3]=<img style={{height:"100px"}} src={img3} alt="img3"className="img"/>
+            img[4]=<img style={{height:"100px"}} src={img4} alt="img4"className="img"/>
+            img[5]=<img  style={{height:"100px"}}src={img5} alt="img5"className="img"/>
+            img[6]=<img style={{height:"100px"}} src={img6} alt="img6"className="img"/>
+    
         this.setState({
             yourDice:(Math.floor(Math.random()*6)+1),
             ComputerDice:(Math.floor(Math.random()*6)+1)},
@@ -35,7 +36,7 @@ class Dice extends React.Component{
                 this.setState({ yourDiceImg:img[this.state.yourDice] })
             this.setState({ComputerDiceImg:img[this.state.ComputerDice]})
             this.setState({game:this.state.game+1})
-        if(this.state.yourDice==null||this.state.ComputerDice==null){
+        if(this.state.yourDice===null||this.state.ComputerDice==null){
             this.setState({
                 count3:null
             })
@@ -43,20 +44,20 @@ class Dice extends React.Component{
        else  if(this.state.yourDice===this.state.ComputerDice){
             this.setState({
                draws:this.state.draws+1,
-               msg:<h3 style={{backgroundColor:"#5d62a8",marginLeft:500,marginRight:500,padding:10,color:"white"}}>Draw</h3>
+               msg:<h3 className="result1">Draw</h3>
             })
         }
          else if(this.state.yourDice>this.state.ComputerDice){
             this.setState({
                 wins:this.state.wins+1,
-                msg:<h3 style={{backgroundColor:"green",marginLeft:500,marginRight:500,padding:10,color:"white"}}>You Won!</h3>
+                msg:<h3  className="result2">You Won!</h3>
              })
         }
     
         else{
             this.setState({
                 lost:this.state.lost+1,
-                msg:<h3 style={{backgroundColor:"#b63c38",marginLeft:500,marginRight:500,padding:10,color:"white"}}>You Lost!</h3>
+                msg:<h3  className="result3">You Lost!</h3>
              })
     }
   }
@@ -84,7 +85,7 @@ Clear=()=>{
            <div><img style={{height:"200px",marginTop:"40px",cursor:"pointer"}} src={download} alt={download} onClick={this.clickHandler}/>
            <span>  <button style={{backgroundColor:'#e65656',color:'white',buttonRadius:'20',cursor:'pointer'}} onClick={this.Clear}>Clear</button></span>
            </div>
-         <h3 style={{backgroundColor:"#8a8af1",marginLeft:500,marginRight:500,padding:10,color:"white"}}><span>You : {this.state.yourDice}</span>
+         <h3 className='hi'><span>You : {this.state.yourDice}</span>
            <span>    Computer : {this.state.ComputerDice}</span></h3>
          <h5> Game : {this.state.game}</h5>
          {this.state.start}
